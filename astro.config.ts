6c907d7dda5f4f0e78396d10a95e896a75407c6f
lialittis <yu-tianchi@outlook.com>
2024-06-10 17:26:13 +0800

    update astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-
+import netlify from "@astrojs/netlify";
 // import sitemap from "@astrojs/sitemap";
 // import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
@@ -13,20 +13,18 @@ import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
-    : 'http://localhost:4321',
+  site: "https://www.tianchiyu.me",
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
-    robotsTxt({
-      sitemap: [
-        "https://lialittis.github.io/sitemap-index.xml",
-        "https://lialittis.github.io/sitemap-0.xml",
-      ],
-    }),
+    // robotsTxt({
+    //   sitemap: [
+    //     "https://lialittis.github.io/sitemap-index.xml",
+    //     "https://lialittis.github.io/sitemap-0.xml",
+    //   ],
+    // }),
     solidJs(),
     UnoCSS({ injectReset: true }),
     icon()
@@ -35,4 +33,5 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime],
   },
   output: "server",
+  adapter: netlify(),
 })

83a724d49b4ec21339145de36d2e5499c5a60179
lialittis <yu-tianchi@outlook.com>
2024-06-10 23:23:50 +0800

    update output as static && keep only pnpm as packagemanager

--- astro.config.ts
+++ astro.config.ts
@@ -7,18 +7,20 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.tianchiyu.me",
+  site: "https://tianchiyu.me",
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
     // robotsTxt({
     //   sitemap: [
     //     "https://lialittis.github.io/sitemap-index.xml",
@@ -32,6 +34,6 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
-  output: "server",
-  adapter: netlify(),
+  output: "static", //"server",
+  // adapter: netlify(),
 })

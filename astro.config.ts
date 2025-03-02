83399f4e155e32f7bed0c61829912e0eda3dcc9e
lialittis <yu-tianchi@outlook.com>
2025-03-02 13:37:31 +0100

    add prettier way

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind'
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
 import sitemap from '@astrojs/sitemap';
-import solidJs from "@astrojs/solid-js";
+// import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 // https://astro.build/config

a925ceb409e62fe8cac481c24289416c6db81901
lialittis <yu-tianchi@outlook.com>
2025-05-28 14:43:37 +0200

    add autoprefixer package

--- archived/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import typography from '@tailwindcss/typography'
-
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [typography],
-}
